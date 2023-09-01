07ad07a1ded08eade3716e62efbf34f16a089353
Angardo18 <48691615+Angardo18@users.noreply.github.com>
2023-09-01 10:53:09 -0600

    Update circuit.v

--- src/circuit.v
+++ src/circuit.v
@@ -1,4 +1,4 @@
-module tt_alu (
+module tt_um_alu (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
