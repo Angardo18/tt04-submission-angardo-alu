c22d55fb5e77263bf5703ed1f853af15fb95e0f6
Angardo18 <48691615+Angardo18@users.noreply.github.com>
2023-09-01 10:38:46 -0600

    Update circuit.v

--- src/circuit.v
+++ src/circuit.v
@@ -8,7 +8,31 @@ module tt_alu (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+    
+    wire [3:0] A,B;
+    reg [7:0] result;
 
+    assign A = ui_in[3:0];
+    assign B = ui_in[7:4];
 
+    assign uo_out = result;
+    
+    assign uio_oe = 8'h00;
+    
+    
+    always@(posedge clk) begin
+
+        case (uio_in)
+
+            8'd0: result = A + B;
+            8'd1: result = A - B;
+            8'd2: result = A*B;
+            8'd3: result = A/B;
+            8'd4: result = {A[3]&B[3],A[2]&B[2],A[1]&B[1],A[0]&B[0]};
+                default: result = {B,A};
+
+        endcase
+
+    end
 
 endmodule
